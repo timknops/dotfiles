281ce5aaea173bc53af98c7a45f53e93a012172c
Tim Knops <timknops22@gmail.com>
2024-07-19 14:01:34 +0200

    Reformatting and further refactoring

---
+++ lua/plugins/indent_blankline.lua
@@ -0,0 +1,21 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  event = 'BufEnter',
+  opts = {
+    indent = {
+      char = '│',
+      tab_char = '│',
+    },
+    scope = { enabled = false },
+    exclude = {
+      filetypes = {
+        'help',
+        'lazy',
+        'mason',
+        'notify',
+        'oil',
+      },
+    },
+  },
+  main = 'ibl',
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,24 @@
-return
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  opts = {
+    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'css', 'go', 'java' },
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { 'ruby' },
     },
-    config = function(_, opts)
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
+    indent = { enable = true, disable = { 'ruby' } },
+  },
+  config = function(_, opts)
+    -- Prefer git instead of curl in order to improve connectivity in some environments
+    require('nvim-treesitter.install').prefer_git = true
 
-      -- Set Clang as the compiler
-      require('nvim-treesitter.install').compilers = { 'clang' }
+    -- Set Clang as the compiler
+    require('nvim-treesitter.install').compilers = { 'clang' }
 
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-    end
-  }
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup(opts)
+  end,
+}
