1b6d90009fbfa6c0bc43f9b801de05d6cad8a145
Tim <timknops22@gmail.com>
2024-09-29 19:51:51 +0200

    Changing wallpapers, adding cycle script, configuring dunst

--- i3-startup/i3_startup.sh
+++ i3-startup/i3_startup.sh
@@ -4,3 +4,17 @@ sleep 2
 setxkbmap -option ctrl:nocaps
 xcape -e 'Control_L=Escape'
 xrandr --output DP-3 --mode 1920x1080 --rate 144.00
+.config/wallpaper_cycle &
+
+dunst &
+
+shutdown -h 23:15
+
+shutdown_time=$(date -d "23:15" +%s)
+current_time=$(date +%s)
+time_until_shutdown=$((shutdown_time - current_time))
+
+if [ $time_until_shutdown -gt 300 ]; then
+    sleep $((time_until_shutdown - 300))
+    notify-send "System Shutdown" "System will shut down in 5 minutes."
+fi
