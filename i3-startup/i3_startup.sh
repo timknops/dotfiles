61cf780ac79bf66dd836278ae9f6a5e8e734cb37
Tim <timknops22@gmail.com>
2024-10-05 21:05:45 +0200

    Adding tmux config and removing wallpapers

--- i3-startup/i3_startup.sh
+++ i3-startup/i3_startup.sh
@@ -3,14 +3,14 @@
 sleep 2
 setxkbmap -option ctrl:nocaps
 xcape -e 'Control_L=Escape'
-xrandr --output DP-3 --mode 1920x1080 --rate 144.00
+xrandr --output DP-2 --mode 1920x1080 --rate 144.00
 .config/wallpaper_cycle &
 
 dunst &
 
-shutdown -h 23:15
+shutdown -h 23:00
 
-shutdown_time=$(date -d "23:15" +%s)
+shutdown_time=$(date -d "23:00" +%s)
 current_time=$(date +%s)
 time_until_shutdown=$((shutdown_time - current_time))
 
--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -41,9 +41,9 @@ return {
       completion = { completeopt = 'menu,menuone,noinsert' },
       mapping = cmp.mapping.preset.insert {
         -- Select the [n]ext item
-        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
         -- Select the [p]revious item
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
 
         -- Scroll the documentation window [b]ack / [f]orward
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -52,7 +52,7 @@ return {
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
         --  This will expand snippets if the LSP sent a snippet.
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<C-l>'] = cmp.mapping.confirm { select = true },
 
         -- If you prefer more traditional completion keymaps,
         -- you can uncomment the following lines
@@ -73,7 +73,7 @@ return {
         --
         -- <c-l> will move you to the right of each of the expansion locations.
         -- <c-h> is similar, except moving you backwards.
-        ['<C-l>'] = cmp.mapping(function()
+        ['<C-y>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           end
