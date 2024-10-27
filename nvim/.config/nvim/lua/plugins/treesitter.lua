b6744a41f93297bb7ef73fe225a18a2a1772e94d
Tim <timknops22@gmail.com>
2024-10-27 12:03:53 +0100

    Small update

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'css', 'go', 'java', 'svelte', 'typescript', 'json' },
+    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'css', 'go', 'java', 'svelte', 'typescript', 'json', 'php' },
     auto_install = true,
     highlight = {
       enable = true,
