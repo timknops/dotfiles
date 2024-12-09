271104f916ebfd9afb29bc64922c44643f922df6
Tim <timknops22@gmail.com>
2024-12-09 19:54:44 +0100

    adjusting font

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,13 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+
   config = function()
     require('lualine').setup {
       options = {
         icons_enabled = true,
         theme = 'gruvbox-material',
-        component_separators = { left = '', right = '|' },
+        component_separators = { left = '|', right = '|' },
       },
       sections = {
         lualine_a = { 'mode' },
