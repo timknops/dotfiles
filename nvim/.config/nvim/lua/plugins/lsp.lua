7b4e32282b984d00fff3ddaed96f8c6852c11728
Tim <timknops22@gmail.com>
2025-09-07 11:36:29 +0200

    fix: adjusting nvim config for 0.11

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,8 +2,9 @@ return {
   'neovim/nvim-lspconfig',
 
   dependencies = {
-    { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-    'williamboman/mason-lspconfig.nvim',
+    -- Pin mason plugins to stable versions to avoid breaking changes
+    { 'williamboman/mason.nvim', version = '~1.10', config = true },
+    { 'williamboman/mason-lspconfig.nvim', version = '~1.29' },
     'WhoIsSethDaniel/mason-tool-installer.nvim',
 
     'nvimtools/none-ls.nvim',
@@ -12,21 +13,22 @@ return {
 
     'hrsh7th/cmp-nvim-lsp',
   },
+
   config = function()
     local null_ls = require 'null-ls'
 
+    -- Null-ls setup (formatters/linters)
     null_ls.setup {
       sources = {
         null_ls.builtins.formatting.black.with {
-          extra_args = {
-            '--line-length',
-            '80',
-          },
+          extra_args = { '--line-length', '80' },
         },
         null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.prettierd,
       },
     }
 
+    -- LSP keymaps
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
@@ -40,17 +42,13 @@ return {
         map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
         map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
         map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        --  NOTE: Disabled because of keymap conflict
-        -- map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
         map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
         map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header.
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-        -- In short: Config for highlighting references of the word under your cursor
         local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+        -- Highlight references
         if client and client.server_capabilities.documentHighlightProvider then
           local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
@@ -58,13 +56,11 @@ return {
             group = highlight_augroup,
             callback = vim.lsp.buf.document_highlight,
           })
-
           vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
             buffer = event.buf,
             group = highlight_augroup,
             callback = vim.lsp.buf.clear_references,
           })
-
           vim.api.nvim_create_autocmd('LspDetach', {
             group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
             callback = function(event2)
@@ -74,85 +70,55 @@ return {
           })
         end
 
-        -- The following autocommand is used to enable inlay hints in your
-        -- code, if the language server you are using supports them
-        --
-        -- This may be unwanted, since they displace some of your code
+        -- Inlay hints toggle (use older API for compatibility)
         if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
           map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.get())
+            vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled(0))
           end, '[T]oggle Inlay [H]ints')
         end
       end,
     })
 
+    -- LSP capabilities for cmp
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-    -- tsserver = {},
-    local servers = {
+    -- LSP servers (for Mason LSP)
+    local lsp_servers = {
       html = {},
       cssls = {},
-      prettierd = {},
       eslint = {},
       tailwindcss = {},
       pyright = {
-        settings = {
-          python = {
-            analysis = {
-              autoImportCompletions = true,
-            },
-          },
-        },
+        settings = { python = { analysis = { autoImportCompletions = true } } },
       },
-      black = {},
       lua_ls = {
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
-            },
-          },
-        },
+        settings = { Lua = { completion = { callSnippet = 'Replace' } } },
       },
     }
 
-    -- Ensure the servers and tools above are installed
-    --  To check the current status of installed tools and/or manually install
-    --  other tools, you can run
-    --    :Mason
+    -- Setup Mason
     require('mason').setup()
 
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-      'black',
-      'isort',
-    })
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    require('mason-lspconfig').setup {
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
+    -- Setup Mason Tool Installer for formatters and other tools
+    require('mason-tool-installer').setup {
+      ensure_installed = {
+        'stylua',
+        'black',
+        'isort',
+        'prettierd',
       },
     }
+
+    -- Setup Mason LSPConfig for LSP servers only
+    require('mason-lspconfig').setup {
+      ensure_installed = vim.tbl_keys(lsp_servers),
+    }
+
+    -- Setup each LSP server manually
+    for name, cfg in pairs(lsp_servers) do
+      cfg.capabilities = vim.tbl_deep_extend('force', {}, capabilities, cfg.capabilities or {})
+      require('lspconfig')[name].setup(cfg)
+    end
   end,
 }
